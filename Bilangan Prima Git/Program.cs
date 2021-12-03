caad645a616c91952c8cbdadbfba258a1deb5bc9
LisandraN08 <lnicoline@student.ciputra.ac.id>
2021-12-03 10:23:49 +0700

    Input angka oleh user

--- Bilangan Prima Git/Program.cs
+++ Bilangan Prima Git/Program.cs
@@ -8,22 +8,7 @@ namespace Bilangan_Prima_Git
         {
             Console.Write("Masukkan angka: ");
             int Bilangan = Convert.ToInt32(Console.ReadLine());
-            int jumlahFaktor = 0;
-            for (int i=1; i<=Bilangan; i++)
-            {
-                if (Bilangan % i == 0)
-                {
-                    jumlahFaktor += 1;
-                }
-            }
-            if (jumlahFaktor==2)
-            {
-                Console.Write("Input merupakan bilangan prima");
-            }
-            else
-            {
-                Console.Write("Input bukan bilangan prima");
-            }
+
         }
     }
 }
